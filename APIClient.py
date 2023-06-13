ed8d769c9e281d6cead7860eac80acd26bf9dbbc
Jonathan <joni7840850@gmail.com>
2023-06-12 19:49:45 -0600

    add method to filter observations

--- APIClient.py
+++ APIClient.py
@@ -19,6 +19,21 @@ class APIClient:
         self._api_key = api_key
         self._api_urls = api_urls
 
+    @staticmethod
+    def _format_url(url : str, api_key : str) -> str:
+        '''
+        This method formats the url to make the request.
+
+        Parameters:
+            url (str): The url to format.
+            api_key (str): The API key.
+
+        Returns:
+            str: The formatted url.
+        '''
+        target_string = '[Aquí va tu Token]'
+        return url.replace(target_string, api_key)
+
     def urls_info(self) -> list[str]:
         '''
         This method returns the info that can be obtained with urls.
@@ -44,12 +59,33 @@ class APIClient:
 
         '''
         url = self._api_urls[key][0]
-        response = requests.get(url)
+        formated_url = self._format_url(url, self._api_key)
+        response = requests.get(formated_url)
         if response.status_code == 200:
             data = json.loads(response.content)
             return data
         else:
-            return None
+            raise Exception('The request failed.')
+        
+    def get_observation(self, key : str) -> dict[int, list[float]]:
+        '''
+        This method returns the observation values.
+
+        Parameters:
+            key (str): The key to select the url.
+
+        Returns:
+            dict[int, list[float]]: The observation values.
+
+        '''
+        data = self.make_request(key)
+        num_series = len(data['Series'])
+        observations = {idx : [] for idx in range(num_series)}
+        for idx,serie in enumerate(data['Series']):
+            for obs in serie['OBSERVATIONS']:
+                observations[idx].append(float(obs['OBS_VALUE']))
+        return observations
+    
         
 if __name__ == '__main__' :
     from urls import urls 
@@ -60,5 +96,6 @@ if __name__ == '__main__' :
     token = get_token()
     client = APIClient(token, urls)
     print(client.urls_info())
-    population_data = client.make_request('population')
+    population = client.get_observation('population')
+    male_female_population = client.get_observation("male_female_population")
     pass
