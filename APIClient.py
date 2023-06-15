d1de45eb4bee51b59d696e33a665f1e4a71690f0
Jonathan <joni7840850@gmail.com>
2023-06-15 16:35:00 -0600

    Add parameter to return data with custom labels

--- APIClient.py
+++ APIClient.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from typing import Dict, Any, Tuple
+from typing import Dict, Any, Tuple, Iterable
 
 
 class APIClient:
@@ -68,24 +68,28 @@ class APIClient:
             raise Exception("The request failed.")
 
     def get_observation(
-        self, key: str
+        self,
+        key: str,
+        obs_keys: Iterable[Any] = None,
     ) -> Tuple[Dict[int, list[float]], Dict[int, list[str]]]:
         """
         This method returns the observation values and dates.
 
         Parameters:
-            data : dict from the request.
+            key (str): The key to select the url.
+            obs_keys (Iterable[Any]): The keys to select the observations.
 
         Returns:
-            Tuple(Dict[int, list[float]], Dict[int, list[str]]):
+            Tuple(Dict[Iterable[Any], list[float]], Dict[Iterable[Any], list[str]]):
             The observation values and dates for each observation.
         """
         data = self.make_request(key)
         num_series = len(data["Series"])
-        observations = {idx: [] for idx in range(num_series)}
-        dates = {idx: [] for idx in range(num_series)}
-
-        for idx, serie in enumerate(data["Series"]):
+        if obs_keys is None:
+            obs_keys = [f"{i}" for i in range(num_series)]
+        observations = {idx: [] for idx in obs_keys}
+        dates = {idx: [] for idx in obs_keys}
+        for idx, serie in zip(obs_keys, data["Series"]):
             for obs in serie["OBSERVATIONS"]:
                 observations[idx].append(float(obs["OBS_VALUE"]))
                 dates[idx].append(obs["TIME_PERIOD"])
@@ -104,5 +108,7 @@ if __name__ == "__main__":
     client = APIClient(token, urls)
     print(client.urls_info())
     population, date1 = client.get_observation("population")
-    male_female_population, date = client.get_observation("male_female_population")
+    male_female_population, date = client.get_observation(
+        "50-54/55-59/60-64/65-69/70-74/male_female_population"
+    )
     pass
