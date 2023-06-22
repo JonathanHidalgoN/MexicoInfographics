ea40a781a26192ea999ed890cac7cb327210c9fe
Jonathan <joni7840850@gmail.com>
2023-06-22 12:19:40 -0600

    Refactor code

---
+++ utils/DataManipulation.py
@@ -0,0 +1,150 @@
+import streamlit as st
+from pandas import DataFrame 
+
+def get_token(path: str = "mex_api.txt") -> str:
+
+    """
+    This function returns the token to make the request to the API.
+
+    Parameters:
+        path (str): The path of the file that contains the token.
+
+    Returns:
+        str: The token.
+    """
+    try:
+        with open(path) as f:
+            token = f.read()
+    except FileNotFoundError:
+        token = st.secrets["mex_api_token"]
+    return token
+
+
+def unfold_population_per_age_request(key: str, api_client, labels: list[str]) -> tuple:
+    """
+    This function unfolds the population per age request.
+    Parameters:
+        key (str): The key of the request.
+        api_client (APIClient): The APIClient object.
+        labels (list[str]): The labels of the data.
+    Returns:
+        tuple: A tuple with the data and the dates.
+    Raises:
+        assertion error if population data and dates are not the same length or dates list is not equal.
+    """
+
+    d_population, dates = api_client.get_observation(key, labels)
+    # Assert all dates and len of data are the same
+    # is this necessary? or expensive?
+    # I think it its because, this checks:
+    # 1. All dates are the same
+    # 2. All data has the same length
+    # So, I think it is necessary to avoid errors, but maybe it is expensive
+    all(dates[labels[0]] == dates[label] for label in labels)
+    return d_population, dates[labels[0]]
+
+def create_age_data_frame(web_variables : dict, client : any) -> DataFrame:
+    """
+    This function creates a dataframe with the population per age.
+    Parameters:
+        web_variables (dict): The web variables.
+        client (any): The APIClient object.
+    Returns:
+        dataframe: The dataframe with the population per age.
+    """
+    population_age_1, date = unfold_population_per_age_request(
+        web_variables["population_age_keys"][0],
+        client,
+        web_variables["population_age_labels_1"],
+    )
+    population_age_2, _ = unfold_population_per_age_request(
+        web_variables["population_age_keys"][1],
+        client,
+        web_variables["population_age_labels_2"],
+    )
+    population_age_3, _ = unfold_population_per_age_request(
+        web_variables["population_age_keys"][2],
+        client,
+        web_variables["population_age_labels_3"],
+    )
+
+    population_age = {**population_age_1, **population_age_2, **population_age_3}
+    dataframe = DataFrame(population_age, index=date)
+    return dataframe
+
+def add_age_data_frame(dataframe: DataFrame) -> DataFrame:
+    """
+    This funcition merge age columns in the dataframe.
+    Parameters:
+        dataframe (dataframe): The dataframe to merge.
+    Returns:
+        dataframe: The dataframe with the merged columns.
+    """
+    age_dataframe = dataframe.copy()
+    col_names = age_dataframe.columns
+    new_col_names = []
+    
+    for idx, col_name in enumerate(col_names):
+        if idx % 2 == 0:
+            tmp = col_name.split(" ")
+            new_col_names.append(tmp[0] + " years")
+    
+    for idx, col_name in enumerate(col_names):
+        if idx % 2 != 0:
+            age_dataframe[new_col_names[idx // 2]] = age_dataframe[col_name] + age_dataframe[col_names[idx - 1]]
+    
+    for col_name in col_names:
+        age_dataframe.drop(col_name, axis=1, inplace=True)
+    
+    return age_dataframe
+
+
+def cut_age_dataframe(
+    start_year: int,
+    end_year: int,
+    start_age: str,
+    end_age: str,
+    original_df: DataFrame,
+    filter: str,
+) -> DataFrame:
+    """
+    This function cuts the age dataframe to the specified years and age.
+    Parameters:
+        start_year (int): The start year.
+        end_year (int): The end year.
+        start_age (str): The start age.
+        end_age (str): The end age.
+        original_df (dataframe): The original dataframe.
+        fitler (str): The filter to apply to the dataframe.
+    Returns:
+        dataframe: The dataframe with the specified years and age.
+    """
+    e_start_age = start_age + " male"
+    e_end_age = end_age + " female"
+    selected_data_frame = original_df.loc[str(start_year) : str(end_year)].loc[
+        :, e_start_age:e_end_age
+    ]
+    if filter == "Sex":
+        return selected_data_frame
+    else:
+        # Working on summing the columns
+        return add_age_data_frame(selected_data_frame)
+
+
+def create_population_age_labels_sex(start: int, stop: int, step: int) -> list[str]:
+    """
+    This function creates the labels for the population per age request.
+    Parameters:
+        start (int): The start age.
+        stop (int): The stop age.
+        step (int): The step age.
+    Returns:
+        list[str]: The labels for the population per age request.
+    """
+    labels = []
+    for i in range(start, stop, step):
+        labels.append(f"{i}-{i+4} male")
+        labels.append(f"{i}-{i+4} female")
+    return labels
+
+
--- web_page.py
+++ web_page.py
@@ -1,291 +1,23 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import asyncio
-
-
-def get_token(path: str = "mex_api.txt") -> str:
-
-    """
-    This function returns the token to make the request to the API.
-
-    Parameters:
-        path (str): The path of the file that contains the token.
-
-    Returns:
-        str: The token.
-    """
-    try:
-        with open(path) as f:
-            token = f.read()
-    except FileNotFoundError:
-        token = st.secrets["mex_api_token"]
-    return token
-
-
-async def population_counter(
-    t: st._DeltaGenerator = st.empty(),
-    children_per_second: float = 0.07,
-    population: int = 126e6,
-) -> None:
-    # This function displays a counter of the population of Mexico.
-
-    # Parameters:
-    #        t (st._DeltaGenerator): The DeltaGenerator object.
-    #        children_per_second (float): The number of children per second.
-    #        population (int): The initial population.
-
-    # Returns:
-    #        None
-
-    current_population = population
-    header_text = "Live population"
-    digit_template = '<div style="display: inline-block; border: 4px solid black; padding: 5px; margin-right: 5px;">{}</div>'
-    st.write(header_text)
-
-    while True:
-        current_population += 1
-
-        digits = [digit_template.format(digit) for digit in str(current_population)]
-
-        t.markdown("".join(digits), unsafe_allow_html=True)
-
-        # time.sleep(children_per_second)
-        r = await asyncio.sleep(children_per_second)
-
-
-def make_population_distribution_plot(c_APIclient) -> None:
-    """
-    This function makes a plot of the population distribution in Mexico.
-    Parameters:
-        c_APIclient (APIClient): The APIClient object.
-    Returns:
-        None
-
-    Raises:
-        assertion error if population data and dates are not the same length.
-    """
-    (
-        male_female_population_data,
-        male_female_population_dates,
-    ) = c_APIclient.get_observation("male_female_population")
-    male_population = male_female_population_data["0"]
-    female_population = male_female_population_data["1"]
-    male_female_population_dates = male_female_population_dates["0"]
-    assert (
-        len(male_female_population_dates)
-        == len(male_population)
-        == len(female_population)
-    )
-
-    population_df = pd.DataFrame(
-        {
-            "Date": male_female_population_dates,
-            "Male": male_population,
-            "Female": female_population,
-        }
-    )
-
-    fig = px.scatter(
-        population_df,
-        x="Date",
-        y=["Male", "Female"],
-        labels={"value": "Population", "variable": "Gender"},
-        title="Population Distribution",
-    )
-
-    fig.update_layout(
-        plot_bgcolor="black",
-        paper_bgcolor="black",
-        font=dict(color="white"),
-        legend=dict(bgcolor="black", bordercolor="white", font=dict(color="white")),
-        xaxis=dict(tickfont=dict(color="white"), tickangle=45),
-        yaxis=dict(tickfont=dict(color="white")),
-    )
-
-    st.plotly_chart(fig)
-
-
-def unfold_population_per_age_request(key: str, api_client, labels: list[str]) -> tuple:
-    """
-    This function unfolds the population per age request.
-    Parameters:
-        key (str): The key of the request.
-        api_client (APIClient): The APIClient object.
-        labels (list[str]): The labels of the data.
-    Returns:
-        tuple: A tuple with the data and the dates.
-    Raises:
-        assertion error if population data and dates are not the same length or dates list is not equal.
-    """
-
-    d_population, dates = api_client.get_observation(key, labels)
-    # Assert all dates and len of data are the same
-    # is this necessary? or expensive?
-    # I think it its because, this checks:
-    # 1. All dates are the same
-    # 2. All data has the same length
-    # So, I think it is necessary to avoid errors, but maybe it is expensive
-    all(dates[labels[0]] == dates[label] for label in labels)
-    return d_population, dates[labels[0]]
-
-
-def create_age_data_frame() -> pd.DataFrame:
-    """
-    This function creates a dataframe with the population per age.
-    Parameters:
-        None
-    Returns:
-        dataframe: The dataframe with the population per age.
-    """
-    population_age_1, date = unfold_population_per_age_request(
-        web_variables["population_age_keys"][0],
-        client,
-        web_variables["population_age_labels_1"],
-    )
-    population_age_2, _ = unfold_population_per_age_request(
-        web_variables["population_age_keys"][1],
-        client,
-        web_variables["population_age_labels_2"],
-    )
-    population_age_3, _ = unfold_population_per_age_request(
-        web_variables["population_age_keys"][2],
-        client,
-        web_variables["population_age_labels_3"],
-    )
-
-    population_age = {**population_age_1, **population_age_2, **population_age_3}
-    dataframe = pd.DataFrame(population_age, index=date)
-    return dataframe
-
-
-def add_age_data_frame(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """
-    This funcition merge age columns in the dataframe.
-    Parameters:
-        dataframe (dataframe): The dataframe to merge.
-    Returns:
-        dataframe: The dataframe with the merged columns.
-    """
-    age_dataframe = dataframe.copy()
-    col_names = age_dataframe.columns
-    new_col_names = []
-    for idx, col_name in enumerate(col_names):
-        if idx % 2 == 0:
-            tmp = col_name.split(" ")
-            new_col_names.append(tmp[0] + " years")
-        for idx, col_name in enumerate(col_names):
-            if idx % 2 != 0:
-                age_dataframe[new_col_names[idx // 2]] = (
-                    age_dataframe[col_name] + age_dataframe[col_names[idx - 1]]
-                )
-        for idx, col_name in enumerate(col_names):
-            age_dataframe.drop(col_name, axis=1, inplace=True)
-        return age_dataframe
-
-
-def cut_age_dataframe(
-    start_year: int,
-    end_year: int,
-    start_age: str,
-    end_age: str,
-    original_df: pd.DataFrame,
-    filter: str,
-) -> pd.DataFrame:
-    """
-    This function cuts the age dataframe to the specified years and age.
-    Parameters:
-        start_year (int): The start year.
-        end_year (int): The end year.
-        start_age (str): The start age.
-        end_age (str): The end age.
-        original_df (dataframe): The original dataframe.
-        fitler (str): The filter to apply to the dataframe.
-    Returns:
-        dataframe: The dataframe with the specified years and age.
-    """
-    e_start_age = start_age + " male"
-    e_end_age = end_age + " female"
-    selected_data_frame = original_df.loc[str(start_year) : str(end_year)].loc[
-        :, e_start_age:e_end_age
-    ]
-    if filter == "Sex":
-        return selected_data_frame
-    else:
-        # Working on summing the columns
-        return add_age_data_frame(selected_data_frame)
-
-
-def add_age_data_frame(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """
-    This function merges age columns in the dataframe.
-    Parameters:
-        dataframe (pd.DataFrame): The dataframe to merge.
-    Returns:
-        pd.DataFrame: The dataframe with the merged columns.
-    """
-    age_dataframe = dataframe.copy()
-    col_names = age_dataframe.columns
-    new_col_names = []
-
-    for idx, col_name in enumerate(col_names):
-        if idx % 2 == 0:
-            tmp = col_name.split(" ")
-            new_col_names.append(tmp[0] + " years")
-
-    for idx, col_name in enumerate(col_names):
-        if idx % 2 != 0:
-            age_dataframe[new_col_names[idx // 2]] = (
-                age_dataframe[col_name] + age_dataframe[col_names[idx - 1]]
-            )
-
-    for col_name in col_names:
-        age_dataframe.drop(col_name, axis=1, inplace=True)
-
-    return age_dataframe
-
-
-def plot_cut_age_dataframe(cut_dataframe: pd.DataFrame) -> None:
-    """
-    This function plots the cut age dataframe.
-    Parameters:
-        cut_dataframe (dataframe): The cut dataframe.
-    Returns:
-        None
-    """
-    fig = px.line(cut_dataframe)
-    fig.update_layout(
-        xaxis_title="Year",
-        yaxis_title="Population",
-        legend_title="Age",
-    )
-    st.plotly_chart(fig)
-
-
-def create_population_age_labels_sex(start: int, stop: int, step: int) -> list[str]:
-    """
-    This function creates the labels for the population per age request.
-    Parameters:
-        start (int): The start age.
-        stop (int): The stop age.
-        step (int): The step age.
-    Returns:
-        list[str]: The labels for the population per age request.
-    """
-    labels = []
-    for i in range(start, stop, step):
-        labels.append(f"{i}-{i+4} male")
-        labels.append(f"{i}-{i+4} female")
-    return labels
-
-
 if __name__ == "__main__":
 
+    import streamlit as st
+    
+    from asyncio import run as asyncio_run
     from urls import urls
     from APIClient import APIClient
+
+    from utils.Visualizations import (
+        plot_cut_age_dataframe,
+        make_population_distribution_plot,
+        population_counter
+    )
+
+    from utils.DataManipulation import (
+        get_token,
+        create_age_data_frame,
+        cut_age_dataframe,
+        create_population_age_labels_sex,
+    )
 
     ################################################################################
     #                                WEB VARIABLES                                 #
@@ -306,7 +38,10 @@ if __name__ == "__main__":
     client = APIClient(web_variables["token"], urls)
     population, _ = client.get_observation("population")
     population = population["0"][0]
-    age_male_female_dataframe = create_age_data_frame()
+    age_male_female_dataframe = create_age_data_frame(
+        web_variables,
+        client
+    )
     population_age_labels = (
         web_variables["population_age_labels_1"]
         + web_variables["population_age_labels_2"]
@@ -396,7 +131,7 @@ if __name__ == "__main__":
         # Async do not work with streamlit, put this in the end of the script,
         # the col will keep in the same place
         t = st.empty()
-        asyncio.run(
+        asyncio_run(
             population_counter(t=t, children_per_second=21, population=population)
         )
 
