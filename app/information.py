ef8ace254ff39a7955156eacb6478f996f41318b
Jonathan <joni7840850@gmail.com>
2023-07-03 11:02:16 -0600

    Add file with information

---
+++ app/information.py
@@ -0,0 +1,53 @@
+introduction = "Welcome to the Mexico Exploration Page! Here, you can embark on an interactive journey to discover and learn about Mexico through engaging \
+    visualizations and graphs. The information presented on this page is sourced from the INEGI (National Institute of Statistics and Geography) \
+    API, ensuring accurate and reliable data. Immerse \
+    yourself in the rich cultural, geographical, and demographic aspects of Mexico with our interactive tools and explore the \
+    fascinating diversity of this vibrant country."
+
+info_1 = (
+    "Mexico (Spanish: México), officially the United Mexican States, is a country in the southern portion of North America. "
+    "It is bordered to the north by the United States; to the south and west by the Pacific Ocean; to the southeast by Guatemala"
+    ", Belize, and the Caribbean Sea; and to the east by the Gulf of Mexico."
+)
+
+info_2 = (
+    "Mēxihco is the Nahuatl term for the heartland of the Aztec Empire, namely the Valley of Mexico \
+    and surrounding territories, with its people being known as the Mexica.\
+    The name came to apply to the territory controlled by the Aztec Empire, which extended from the Atlantic Ocean to the Pacific Ocean, "
+    "from central Mexico southwards. "
+)
+
+state_descriptions = [
+    "Zacatecas: Located in north-central Mexico, known for its rich mining history and beautiful colonial architecture.",
+    "Yucatán: Situated on the Yucatán Peninsula, famous for its Mayan ruins like Chichen Itza and stunning cenotes (natural sinkholes).",
+    "Veracruz: Located on the Gulf of Mexico, known for its vibrant culture, beautiful beaches, and historical significance.",
+    "Tlaxcala: The smallest state in Mexico, known for its indigenous culture and historic sites.",
+    "Tamaulipas: Situated along the Gulf of Mexico, known for its diverse landscapes and vibrant economy.",
+    "Tabasco: Located in southeastern Mexico, known for its lush jungles, archaeological sites, and hot climate.",
+    "Sonora: Located in northwestern Mexico, known for its desert landscapes and mining activities.",
+    "Sinaloa: Situated on the western coast, known for its beautiful beaches, agriculture, and music.",
+    "San Luis Potosí: Located in north-central Mexico, known for its rich history, nature reserves, and diverse culture.",
+    "Quintana Roo: Situated on the Yucatán Peninsula, famous for its popular tourist destinations like Cancún and Playa del Carmen.",
+    "Querétaro: Located in central Mexico, known for its colonial architecture, vineyards, and vibrant culture.",
+    "Puebla: Known for its rich culinary tradition, historical sites like Cholula, and the iconic Popocatépetl volcano.",
+    "Oaxaca: Situated in southern Mexico, known for its indigenous cultures, arts, and the ancient ruins of Monte Albán.",
+    "Nuevo León: Located in northeastern Mexico, known for its industrialization, modern cities, and natural attractions.",
+    "Nayarit: Located along the Pacific coast, known for its stunning beaches, tropical forests, and diverse marine life.",
+    "Morelos: Situated near Mexico City, known for its pleasant climate, historical sites, and natural beauty.",
+    "Michoacán: Located in western Mexico, known for its picturesque colonial towns, natural reserves, and cultural heritage.",
+    "México: Surrounding Mexico City, known for its urban sprawl, cultural attractions, and historical significance.",
+    "Jalisco: Situated in western Mexico, known for its tequila production, mariachi music, and Guadalajara city.",
+    "Hidalgo: Located in central Mexico, known for its mining history, mountainous landscapes, and unique cuisine.",
+    "Guerrero: Situated on the Pacific coast, known for its beautiful beaches, vibrant culture, and Acapulco city.",
+    "Guanajuato: Located in central Mexico, known for its colonial architecture, silver mines, and cultural festivals.",
+    "Durango: Known for its rugged landscapes, Western film history, and mining activities.",
+    "Ciudad de México: The capital of Mexico, known for its rich history, museums, and bustling urban life.",
+    "Colima: Situated along the Pacific coast, known for its volcanoes, beaches, and pleasant climate.",
+    "Coahuila: Located in northern Mexico, known for its diverse landscapes, including deserts, forests, and mountains.",
+    "Chihuahua: Mexico's largest state, known for its stunning Copper Canyon, desert landscapes, and cowboy culture.",
+    "Chiapas: Located in southern Mexico, known for its indigenous cultures, jungles, and ancient ruins like Palenque.",
+    "Campeche: Situated on the Yucatán Peninsula, known for its well-preserved colonial architecture and Mayan ruins.",
+    "Baja California Sur: Located on the Baja California Peninsula, known for its beautiful beaches, marine life, and Los Cabos.",
+    "Baja California: Situated on the Baja California Peninsula, known for its unique desert landscapes, vineyards, and outdoor activities.",
+    "Aguascalientes: Known for its annual San Marcos Fair, hot springs, and vibrant cultural scene.",
+]
